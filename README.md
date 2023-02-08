fde2aa8d9c8d339fcca310ab8cbcca746a4a56bc
Saúl Piña <sauljabin@gmail.com>
2023-02-08 14:17:37 -0500

    add script

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # kafka-cli-installer
 Shell script to install kafka cli
+
+Installation:
+```shell
+curl -s "https://raw.githubusercontent.com/sauljabin/kafka-cli-installer/main/install.sh" | bash
+```
---
+++ install.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+CONFLUENT_VERSION="7.3"
+CONFLUENT_FULL_VERSION="7.3.1"
+CONFLUENT_URL="http://packages.confluent.io/archive/$CONFLUENT_VERSION/confluent-community-$CONFLUENT_FULL_VERSION.zip"
+CONFLUENT_HOME="$HOME/.confluent"
+CONFLUENT_BIN="$CONFLUENT_HOME/bin"
+
+wget -q "$CONFLUENT_URL" -O /tmp/confluent.zip
+unzip -q -o /tmp/confluent.zip -d /tmp
+rm -rf "$CONFLUENT_HOME"
+mv "/tmp/confluent-$CONFLUENT_FULL_VERSION" "$CONFLUENT_HOME"
+
+NEW_PATH="export PATH=\"\$PATH:$CONFLUENT_BIN\""
+
+echo "$NEW_PATH" >> ~/.zshrc
+echo "$NEW_PATH" >> ~/.bashrc
+eval "$NEW_PATH"
